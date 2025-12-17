0d1683d1244ccd1d4258fda0b06ff332c3ed0ea2
Rebecca Stjärnsten <rebecca.stjarnsten@learnet.se>
2025-12-17 11:51:14 +0100

    Secrets och migration

--- DbContext/Migrations/SqlServerDbContext/20251217105038_miInitial.Designer.cs
+++ DbContext/Migrations/SqlServerDbContext/20251217105038_miInitial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DbContext.Migrations.SqlServerDbContext
 {
     [DbContext(typeof(MainDbContext.SqlServerDbContext))]
-    [Migration("20251205090631_miInitial")]
+    [Migration("20251217105038_miInitial")]
     partial class miInitial
     {
         /// <inheritdoc />
